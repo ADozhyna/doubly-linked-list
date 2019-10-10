c7ee64b090e0936f835b844b78be59e88aab0013
Hanna Dozhyna <annadozhyna@gmail.com>
2019-10-10 22:44:26 +0300

    add part of solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,75 @@
-const Node = require('./node');
+const Node = require("./node");
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.length = 0;
+    this._head = null;
+    this._tail = null;
+  }
 
-    append(data) {}
+  append(data) {
+    let node = new Node(data);
 
-    head() {}
+    if (this.length) {
+      node.prev = this._tail;
+      this._tail.next = node;
+      this._tail = node;
+    } else {
+      this._head = node;
+      this._tail = node;
+    }
+    this.length++;
+    return node;
+  }
 
-    tail() {}
+  head() {
+    return this._head ? this._head.data : this._head;
+  }
 
-    at(index) {}
+  tail() {
+    return this._tail ? this._tail.data : this._tail;
+  }
 
-    insertAt(index, data) {}
+  at(index) {
+    let currentNode = this._head;
+    let i = 0;
+    while (i !== index) {
+      if (index === 0) {
+        return this._head.data;
+      } else if (index === this.length - 1) {
+        return this._tail.data;
+      }
+      currentNode = currentNode.next;
+      i++;
+    }
+    return currentNode.data;
+  }
 
-    isEmpty() {}
+  insertAt(index, data) {
+    //вставить данные в указанный индекс
+  }
 
-    clear() {}
+  isEmpty() {
+    if (!this.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-    deleteAt(index) {}
+  clear() {
+    this._head = null;
+    this._tail = null;
+    this.length = 0;
 
-    reverse() {}
+    return this;
+  }
 
-    indexOf(data) {}
+  deleteAt(index) {}
+
+  reverse() {}
+
+  indexOf(data) {}
 }
 
 module.exports = LinkedList;
